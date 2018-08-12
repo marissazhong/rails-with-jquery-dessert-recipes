ee1098aa6ef6121e3614f858cdacdb342ec441b8
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-12 18:52:34 -0400

    update seed data to correct format

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,26 +68,26 @@ recipe3 = {name: "Pecan Pie", prep_time: 20, cook_time: 60, ingredients_recipes_
 
 
 recipe4 = {name: "Chocolate Babka", prep_time: 150, cook_time: 50, ingredients_recipes_attributes: {
-    "0": {name: "milk", quantity: "1/2 cup"},
-    "1": {name: "active dry yeast", quantity: "1 pkg"},
-    "2": {name: "sugar", quantity: "1/3 cup"},
-    "3": {name: "flour", quantity: "4-1/4 cups"},
-    "4": {name: "salt", quantity: "1-1/2 cup"},
-    "5": {name: "vanilla extract", quantity: "1 tsp"},
-    "6": {name: "nutmeg", quantity: "1/2 tsp"},
-    "7": {name: "egg", quantity: "4 ct"},
-    "8": {name: "butter", quantity: "10 tbs"},
-    "9": {name: "sugar", quantity: "1/2 cup"},
-    "10": {name: "heavy cream", quantity: "3/4 cup"},
-    "11": {name: "chocolate", quantity: "6 oz"},
-    "12": {name: "butter", quantity: "1/2 cup"},
-    "13": {name: "vanilla extract", quantity: "2 tsp"},
-    "14": {name: "flour", quantity: "1/2 cup"},
-    "15": {name: "sugar", quantity: "3 tbs"},
-    "16": {name: "cocoa powder", quantity: "1-1/2 tbs"},
-    "17": {name: "salt", quantity: "1/2 tsp"},
-    "18": {name: "butter", quantity: "4-1/2 tbs"},
-    "19": {name: "chocolate chips", quantity: "1/3 cup"}
+    "0": {input_name: "milk", quantity: "1/2 cup"},
+    "1": {input_name: "active dry yeast", quantity: "1 pkg"},
+    "2": {input_name: "sugar", quantity: "1/3 cup"},
+    "3": {input_name: "flour", quantity: "4-1/4 cups"},
+    "4": {input_name: "salt", quantity: "1-1/2 cup"},
+    "5": {input_name: "vanilla extract", quantity: "1 tsp"},
+    "6": {input_name: "nutmeg", quantity: "1/2 tsp"},
+    "7": {input_name: "egg", quantity: "4 ct"},
+    "8": {input_name: "butter", quantity: "10 tbs"},
+    "9": {input_name: "sugar", quantity: "1/2 cup"},
+    "10": {input_name: "heavy cream", quantity: "3/4 cup"},
+    "11": {input_name: "chocolate", quantity: "6 oz"},
+    "12": {input_name: "butter", quantity: "1/2 cup"},
+    "13": {input_name: "vanilla extract", quantity: "2 tsp"},
+    "14": {input_name: "flour", quantity: "1/2 cup"},
+    "15": {input_name: "sugar", quantity: "3 tbs"},
+    "16": {input_name: "cocoa powder", quantity: "1-1/2 tbs"},
+    "17": {input_name: "salt", quantity: "1/2 tsp"},
+    "18": {input_name: "butter", quantity: "4-1/2 tbs"},
+    "19": {input_name: "chocolate chips", quantity: "1/3 cup"}
 }, directions: {
     "0": "Prepare the dough: In a small saucepan or a bowl in the microwave, warm the milk until it’s lukewarm but not hot (about 110 degrees). Add yeast and a pinch of sugar and let sit for 5 to 10 minutes, until slightly foamy.",
     "1": "In an electric mixer fitted with the dough hook, or in a food processor, mix together flour, 1/3 cup sugar, the salt, the vanilla, the lemon zest (if using) and the nutmeg. (If you don't have a mixer or processor, use a large bowl and a wooden spoon.) Beat or process in the yeast mixture and eggs until the dough comes together in a soft mass, about 2 minutes. If the dough sticks to the side of the bowl and doesn’t come together, add a tablespoon more flour at a time until it does, beating very well in between additions.",
@@ -103,10 +103,10 @@ recipe4 = {name: "Chocolate Babka", prep_time: 150, cook_time: 50, ingredients_r
     "11": "When you're ready to bake, heat the oven to 350 degrees. Use your fingers to clump streusel together and scatter all over the tops of the cakes. Transfer to oven and bake until a tester goes into the cakes without any rubbery resistance and comes out clean, 40 to 50 minutes. The cakes will also sound hollow if you unmold them and tap on the bottom. An instant-read thermometer will read between 185 and 210 degrees."}}
     
 recipe5 = {name: "Macaron", prep_time: 150, cook_time: 10, ingredients_recipes_attributes: {
-    "0": {name: "egg white", quantity: "3 ct"},
-    "1": {name: "sugar", quantity: "1/4 cup"},
-    "2": {name: "powdered sugar", quantity: "1-2/3 cups"},
-    "3": {name: "almond flour", quantity: "1 cup"}
+    "0": {input_name: "egg white", quantity: "3 ct"},
+    "1": {input_name: "sugar", quantity: "1/4 cup"},
+    "2": {input_name: "powdered sugar", quantity: "1-2/3 cups"},
+    "3": {input_name: "almond flour", quantity: "1 cup"}
 }, directions: {
     "0": "Beat egg whites in the bowl of a stand mixer fitted with a whisk attachment until whites are foamy; beat in white sugar and continue beating until egg whites are glossy, fluffy, and hold soft peaks. Sift confectioners' sugar and ground almonds in a separate bowl and quickly fold the almond mixture into the egg whites, about 30 strokes.",
     "1": "Spoon a small amount of batter into a plastic bag with a small corner cut off and pipe a test disk of batter, about 1 1/2 inches in diameter, onto prepared baking sheet. If the disk of batter holds a peak instead of flattening immediately, gently fold the batter a few more times and retest.",
@@ -114,46 +114,46 @@ recipe5 = {name: "Macaron", prep_time: 150, cook_time: 10, ingredients_recipes_a
     "3": "Preheat oven to 285 degrees F (140 degrees C). Bake cookies until set but not browned, about 10 minutes; let cookies cool completely before filling."}}
 
 recipe6 = {name: "French Meringue", prep_time: 20, cook_time: 180, ingredients_recipes_attributes: {
-    "0": {name: "egg white", quantity: "4 ct"},
-    "1": {name: "powdered sugar", quantity: "2-1/4 cups"}
+    "0": {input_name: "egg white", quantity: "4 ct"},
+    "1": {input_name: "powdered sugar", quantity: "2-1/4 cups"}
 }, directions: {
     "0": "Preheat the oven to 200 degrees F (95 degrees C). Butter and flour a baking sheet.",
     "1": "In a glass or metal bowl, whip egg whites until foamy using an electric mixer. Sprinkle in sugar a little at a time, while continuing to whip at medium speed. When the mixture becomes stiff and shiny like satin, stop mixing, and transfer the mixture to a large pastry bag. Pipe the meringue out onto the prepared baking sheet using a large round tip or star tip.",
     "2": "Place the meringues in the oven and place a wooden spoon handle in the door to keep it from closing all the way. Bake for 3 hours, or until the meringues are dry, and can easily be removed from the pan. Allow cookies to cool completely before storing in an airtight container at room temperature."}},
     
 recipe7 = {name: "Brownies", prep_time: 30, cook_time: 30, ingredients_recipes_attributes: {
-    "0": {name: "butter", quantity: "1/2 cup"},
-    "1": {name: "sugar", quantity: "1 cup"},
-    "2": {name: "egg", quantity: "2 ct"},
-    "3": {name: "vanilla extract", quantity: "1 tsp"},
-    "4": {name: "cocoa powder", quantity: "1/3 cup"},
-    "5": {name: "flour", quantity: "1/2 cup"},
-    "6": {name: "salt", quantity: "1/4 salt"},
-    "7": {name: "baking powder", quantity: "1/4 cup"}
+    "0": {input_name: "butter", quantity: "1/2 cup"},
+    "1": {input_name: "sugar", quantity: "1 cup"},
+    "2": {input_name: "egg", quantity: "2 ct"},
+    "3": {input_name: "vanilla extract", quantity: "1 tsp"},
+    "4": {input_name: "cocoa powder", quantity: "1/3 cup"},
+    "5": {input_name: "flour", quantity: "1/2 cup"},
+    "6": {input_name: "salt", quantity: "1/4 salt"},
+    "7": {input_name: "baking powder", quantity: "1/4 cup"}
 }, directions: {
     "0": "Preheat oven to 350 degrees F (175 degrees C). Grease and flour an 8-inch square pan.",
     "1": "In a large saucepan, melt 1/2 cup butter. Remove from heat, and stir in sugar, eggs, and 1 teaspoon vanilla. Beat in 1/3 cup cocoa, 1/2 cup flour, salt, and baking powder. Spread batter into prepared pan.",
     "2": "Bake in preheated oven for 25 to 30 minutes."}}
 
 recipe8 = {name: "Crepes", prep_time: 15, cook_time: 20, ingredients_recipes_attributes: {
-    "0": {name: "flour", quantity: "1 cup"},
-    "1": {name: "egg", quantity: "2 ct"},
-    "2": {name: "milk", quantity: "1/2 cup"},
-    "3": {name: "water", quantity: "1/2 cup"},
-    "4": {name: "salt", quantity: "1/4 tsp"},
-    "5": {name: "butter", quantity: "2 tbs"}            
+    "0": {input_name: "flour", quantity: "1 cup"},
+    "1": {input_name: "egg", quantity: "2 ct"},
+    "2": {input_name: "milk", quantity: "1/2 cup"},
+    "3": {input_name: "water", quantity: "1/2 cup"},
+    "4": {input_name: "salt", quantity: "1/4 tsp"},
+    "5": {input_name: "butter", quantity: "2 tbs"}            
 }, directions: {
     "0": "In a large mixing bowl, whisk together the flour and the eggs. Gradually add in the milk and water, stirring to combine. Add the salt and butter; beat until smooth.",
     "1": "Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each crepe. Tilt the pan with a circular motion so that the batter coats the surface evenly.",
     "2": "Cook the crepe for about 2 minutes, until the bottom is light brown. Loosen with a spatula, turn and cook the other side. Serve hot."}}
 
 recipe9 = {name: "Sugar Coated Pecans", prep_time: 15, cook_time: 60, ingredients_recipes_attributes: {
-    "0": {name: "egg white", quantity: "1 ct"},
-    "1": {name: "water", quantity: "1 tbs"},
-    "2": {name: "pecans", quantity: "1 lb"},
-    "3": {name: "sugar", quantity: "1 cup"},
-    "4": {name: "salt", quantity: "3/4 tsp"},
-    "5": {name: "cinnamon", quantity: "1/2 tsp"}
+    "0": {input_name: "egg white", quantity: "1 ct"},
+    "1": {input_name: "water", quantity: "1 tbs"},
+    "2": {input_name: "pecans", quantity: "1 lb"},
+    "3": {input_name: "sugar", quantity: "1 cup"},
+    "4": {input_name: "salt", quantity: "3/4 tsp"},
+    "5": {input_name: "cinnamon", quantity: "1/2 tsp"}
 }, directions: {
     "0": "Preheat oven to 250 degrees F (120 degrees C). Grease one baking sheet. ",
     "1": "In a mixing bowl, whip together the egg white and water until frothy. In a separate bowl, mix together sugar, salt, and cinnamon.",
@@ -161,13 +161,13 @@ recipe9 = {name: "Sugar Coated Pecans", prep_time: 15, cook_time: 60, ingredient
     "3": "Bake at 250 degrees F (120 degrees C) for 1 hour. Stir every 15 minutes."}}
 
 recipe10 = {name: "Challah Bread", prep_time: 30, cook_time: 40, ingredients_recipes_attributes: {
-    "0": {name: "water", quantity: "2-1/2 cups"},
-    "1": {name: "active dry yeast", quantity: "1 tbs"},
-    "2": {name: "honey", quantity: "1/2 cup"},
-    "3": {name: "vegetable oil", quantity: "4 tbs"},
-    "4": {name: "egg", quantity: "3 ct"},
-    "5": {name: "salt", quantity: "1 tbs"},
-    "6": {name: "flour", quantity: "8 cups"}            
+    "0": {input_name: "water", quantity: "2-1/2 cups"},
+    "1": {input_name: "active dry yeast", quantity: "1 tbs"},
+    "2": {input_name: "honey", quantity: "1/2 cup"},
+    "3": {input_name: "vegetable oil", quantity: "4 tbs"},
+    "4": {input_name: "egg", quantity: "3 ct"},
+    "5": {input_name: "salt", quantity: "1 tbs"},
+    "6": {input_name: "flour", quantity: "8 cups"}            
 }, directions: {
     "0": "In a large bowl, sprinkle yeast over barely warm water. Beat in honey, oil, 2 eggs, and salt. Add the flour one cup at a time, beating after each addition, graduating to kneading with hands as dough thickens. Knead until smooth and elastic and no longer sticky, adding flour as needed. Cover with a damp clean cloth and let rise for 1 1/2 hours or until dough has doubled in bulk.",
     "1": "Punch down the risen dough and turn out onto floured board. Divide in half and knead each half for five minutes or so, adding flour as needed to keep from getting sticky. Divide each half into thirds and roll into long snake about 1 1/2 inches in diameter. Pinch the ends of the three snakes together firmly and braid from middle. Either leave as braid or form into a round braided loaf by bringing ends together, curving braid into a circle, pinch ends together. Grease two baking trays and place finished braid or round on each. Cover with towel and let rise about one hour.",
